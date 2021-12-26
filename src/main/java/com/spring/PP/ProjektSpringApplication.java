f48760abb01da306247e10f756949ad085336c07
xVulpeSx <jeremi.lisek@gmail.com>
2021-12-26 11:08:26 +0100

    init

---
+++ src/main/java/com/spring/PP/ProjektSpringApplication.java
@@ -0,0 +1,13 @@
+package com.spring.PP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/PP/ProjektSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.PP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

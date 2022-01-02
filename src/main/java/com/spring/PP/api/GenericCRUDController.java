0a750ed7a12393fe1ed78e9a51064241a69a4c7e
jlisek <jlisek@man.poznan.pl>
2022-01-02 18:58:20 +0100

    removed unused imports

--- src/main/java/com/spring/PP/api/GenericCRUDController.java
+++ src/main/java/com/spring/PP/api/GenericCRUDController.java
@@ -10,7 +10,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.nio.charset.MalformedInputException;
 import java.util.List;
 
 @AllArgsConstructor
